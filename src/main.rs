5e954fb7f8bf91d0c5c533ba0e6cc11d3cea6cce
Benbellah-Owino <owinobenbellah@gmail.com>
2026-03-04 18:46:29 +0300

    Add tracing andcomplete wallet creation

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
-use bip39::{Language, Mnemonic};
-use bip32::{DerivationPath, Prefix, PrivateKey, PublicKey, XPrv, secp256k1::elliptic_curve::bigint::ByteArray};
+use ketochain::wallet::Wallet;
+use tracing::{info,trace, debug,warn, error, span, Level };
+use tracing::subscriber;
 fn main() {
-    let mut rng = bip39::rand::thread_rng();
-    let mnemonic = Mnemonic::generate_in_with(&mut rng, Language::English, 24).unwrap();
+    // #region:     -- Setup 
+    tracing_subscriber::fmt().with_max_level(Level::DEBUG).init();// stdout is now subscribed to our logs
+    // #endregion:  -- Setup 
 
-    let seed = mnemonic.to_seed("");
+    info!("Welcome to Keto chain");
 
-    println!("{:?}", seed);
-
-    let master_key = XPrv::new(seed).unwrap();
-    
-    println!("{:?}", master_key.private_key());
-
-    let child_path = "m/0/2147483647'/1/2147483646'";
-    let child_xprv = XPrv::derive_from_path(seed, &child_path.parse().unwrap()).unwrap();
-
-    let child_xpub = child_xprv.public_key();
-
-    let child_xprv_str= child_xprv.to_string(Prefix::XPRV);
-    let child_xpub_str = child_xpub.to_string(Prefix::XPUB);
-    println!("\n\n====== Private Public Key Pair=========");    
-    println!("Child xprv:  {:?}", child_xprv_str);
-    println!("Child xpub:  {:?}", child_xpub_str);
-
-    let signing_key = child_xprv.private_key();
-    let verification_key = child_xpub.public_key();
-
-    use bip32::secp256k1::ecdsa::{
-    signature::{Signer, Verifier},
-    Signature
-};
-
-    let example_msg = b"Hello world";
-    let signature: Signature = signing_key.sign(example_msg);
-
-    let ver = verification_key.verify(example_msg, &signature).is_ok();
-    println!("Is message okay? \"{ver}\"")
+    let wallet = Wallet::new("password");
+    let address = wallet.get_address();
+    debug!("Address is {address}");
 }
+
+    // #region:     -- 
+    // #endregion:  -- 
--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -1,23 +1,37 @@
 // section:     -- Imports
-use bip32::{DerivationPath, ExtendedPrivateKey, ExtendedPublicKey, Prefix, PrivateKey, PublicKey, XPrv, secp256k1::{Secp256k1, ecdsa::{SigningKey, VerifyingKey}, elliptic_curve::bigint::ByteArray}};
+use bip32::{DerivationPath, ExtendedPrivateKey, ExtendedPublicKey, Prefix, PrivateKey, PublicKey, XPrv, secp256k1::{Secp256k1, ecdsa::{SigningKey, VerifyingKey}, elliptic_curve::{bigint::ByteArray, zeroize::Zeroizing}}};
 use bip39::{Language, Mnemonic};
+use sha2::{Sha256, Digest};
+use ripemd::Ripemd160;
+
 use rand::rngs::OsRng;
     use bip32::secp256k1::ecdsa::{
     signature::{Signer, Verifier},
     Signature};
+use tracing::instrument;
+
+    
 // endection:   -- Imports
 
 // section:     -- Struct, Enum & Type definitions
+type KetoMasterKey = Zeroizing<String>;
+
 type KetoPrivateKey =  ExtendedPrivateKey<SigningKey>;
 type KetoPublicKey =  ExtendedPublicKey<VerifyingKey>;
+
+type KetoSigningKey = SigningKey;
+type KetoVerifyingKey = VerifyingKey;
+
+
 #[derive(Clone, Debug)]
 pub struct Wallet {
     address: String,
     private_key:  KetoPrivateKey,
     public_key: KetoPublicKey,
-    master_key: String,
+    master_key: KetoMasterKey,
     mnemonic: Mnemonic,
     seed:[u8; 64],
+    keys:i32,
     balance: u32, //In terms keto sumun
 }
 // endection:   -- Struct and Enum definitions
@@ -28,15 +42,22 @@ pub enum WalletErrors {}
 
 // Public API
 impl Wallet {
+    #[instrument]
     pub fn new(passphrase: &str) -> Wallet {
-        let mut rng = bip39::rand::thread_rng();
+        let mut rng = bip39::rand::thread_rng(); // A random number generator to be used to generate a mmemonic
+        // Below we generate a 24 word mmemonic in english
         let mnemonic = Mnemonic::generate_in_with(&mut rng, Language::English, 24).unwrap();
         
-        let seed = mnemonic.to_seed(passphrase); // passphrase is a string. It can take the value of "" if not provided by user
+        // Converts mnemonic seed to bytes. The passphrase is a string. It can take the value of "" if not provided by user
+        let seed = mnemonic.to_seed(passphrase); // 
         
+        let master_key =  XPrv::new(seed).unwrap();
+        
+        // The derivation path for private key creation
         let path = "m/44'/9999'/0'/0/0";
+        // Create the private key
         let xprv = XPrv::derive_from_path(seed, &path.parse().unwrap()).unwrap();
-
+        // Create public key from  
         let xpub = xprv.public_key();
         
         let xprv_str = xprv.to_string(Prefix::XPRV);
@@ -47,22 +68,24 @@ impl Wallet {
         println!("xpub:  {:?}", xpub_str);
 
 
+        // 
         let signing_key = xprv.private_key();
         let verfication_key = xpub.public_key();
 
     
         Wallet {
-            address: unimplemented!(),
+            address: Self::derive_address(&xpub),
             public_key: xpub,
             private_key: xprv,
-            master_key: unimplemented!(),
+            master_key: xprv_str,
             mnemonic,
             seed,
+            keys: 0,
             balance: 0,
         }
     }
 
-    pub fn getBalance(&self) -> u32 {
+    pub fn get_balance(&self) -> u32 {
         return self.balance;
     }
 
@@ -70,8 +93,8 @@ impl Wallet {
         return &self.address;
     }
 
-    pub fn get_public_key(&self) -> &KetoPublicKey {
-        return &self.public_key;
+    pub fn get_public_key(&self) -> String {
+        return self.public_key.to_string(Prefix::XPRV);
     }
 
     pub fn send_to(&mut self, address: &str) {
@@ -82,8 +105,29 @@ impl Wallet {
     pub fn get_wallet(&self, address: &str) -> Result<Wallet, WalletErrors> {
         unimplemented!()
     }
+
 }
 
 // Implement private API
-impl Wallet {}
+impl Wallet {
+    #[instrument]
+    pub fn derive_address(xpub: &KetoPublicKey) -> String {
+        // Get compressed public key bytes (33 bytes)
+        let pub_key_bytes = xpub.public_key().to_bytes();
+
+        // Double hashing our compressed public key bytes for extra security
+        let sha256_hash = Sha256::digest(&pub_key_bytes);
+        let ripemd_hash = Ripemd160::digest(&sha256_hash);
+
+        // prepending the version byte.[ Bitcoin mainnet's is 0x00, ours is 0x4B ('K' for Keto)]
+        let version_byte: u8 = 0x4B;
+        println!("{version_byte}");
+        let mut payload = vec![version_byte];
+        payload.extend_from_slice(&ripemd_hash);
+
+        // Encode in  Base58Check. This appends a 4-byte checksum (double-SHA256) automatically
+        bs58::encode(payload).with_check().into_string()
+    }
+
+}
 // endection:   -- Struct and Enum declarations
