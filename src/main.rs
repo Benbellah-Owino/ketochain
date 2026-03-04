2eb708611fa24b04a4c4b4c8f4f573c74d38e43d
Benbellah-Owino <owinobenbellah@gmail.com>
2026-03-04 21:20:19 +0300

    Add error handling to wallet creation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use ketochain::wallet::Wallet;
 use tracing::{info,trace, debug,warn, error, span, Level };
 use tracing::subscriber;
+use tracing_subscriber::fmt::format::FmtSpan;
 fn main() {
     // #region:     -- Setup 
-    tracing_subscriber::fmt().with_max_level(Level::DEBUG).init();// stdout is now subscribed to our logs
+    tracing_subscriber::fmt()
+    .with_span_events(FmtSpan::FULL)
+    .with_max_level(Level::DEBUG)
+    .init();// stdout is now subscribed to our logs
     // #endregion:  -- Setup 
 
-    info!("Welcome to Keto chain");
 
-    let wallet = Wallet::new("password");
+    let wallet = Wallet::new("password").unwrap();
     let address = wallet.get_address();
-    debug!("Address is {address}");
+    info!("Welcome to Keto chain. Your new address is {address}");
+    eprintln!("{:#?}",wallet )
 }
 
     // #region:     -- 
--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -1,6 +1,6 @@
 // section:     -- Imports
 use bip32::{DerivationPath, ExtendedPrivateKey, ExtendedPublicKey, Prefix, PrivateKey, PublicKey, XPrv, secp256k1::{Secp256k1, ecdsa::{SigningKey, VerifyingKey}, elliptic_curve::{bigint::ByteArray, zeroize::Zeroizing}}};
-use bip39::{Language, Mnemonic};
+use bip39::{self, Language, Mnemonic};
 use sha2::{Sha256, Digest};
 use ripemd::Ripemd160;
 
@@ -22,6 +22,29 @@ type KetoPublicKey =  ExtendedPublicKey<VerifyingKey>;
 type KetoSigningKey = SigningKey;
 type KetoVerifyingKey = VerifyingKey;
 
+// region Errors
+#[derive(Debug, Clone)]
+pub enum WalletError{
+    // Creation Errors
+    WalletCreationFailed,
+    WalletGenerationError,
+    BIP39Error(bip39::Error),
+    BIP32Error(bip32::Error)
+}
+
+// From
+impl From<bip39::Error> for WalletError{
+    fn from(value: bip39::Error) -> Self {
+        WalletError::BIP39Error(value)
+    }
+}
+
+impl From<bip32::Error> for WalletError{
+    fn from(value: bip32::Error) -> Self {
+        WalletError::BIP32Error(value)
+    }
+}
+// endregion Errors
 
 #[derive(Clone, Debug)]
 pub struct Wallet {
@@ -31,57 +54,34 @@ pub struct Wallet {
     master_key: KetoMasterKey,
     mnemonic: Mnemonic,
     seed:[u8; 64],
-    keys:i32,
+    addresses:i32,
     balance: u32, //In terms keto sumun
 }
 // endection:   -- Struct and Enum definitions
 
-pub enum WalletErrors {}
 
 // section:     -- Struct and Enum declarations
 
 // Public API
 impl Wallet {
-    #[instrument]
-    pub fn new(passphrase: &str) -> Wallet {
+    #[instrument(skip(passphrase))]
+    pub fn new(passphrase: &str) -> Result<Wallet, WalletError> {
         let mut rng = bip39::rand::thread_rng(); // A random number generator to be used to generate a mmemonic
         // Below we generate a 24 word mmemonic in english
-        let mnemonic = Mnemonic::generate_in_with(&mut rng, Language::English, 24).unwrap();
+        let mnemonic = Mnemonic::generate_in_with(&mut rng, Language::English, 24)?;
         
-        // Converts mnemonic seed to bytes. The passphrase is a string. It can take the value of "" if not provided by user
-        let seed = mnemonic.to_seed(passphrase); // 
-        
-        let master_key =  XPrv::new(seed).unwrap();
-        
-        // The derivation path for private key creation
-        let path = "m/44'/9999'/0'/0/0";
-        // Create the private key
-        let xprv = XPrv::derive_from_path(seed, &path.parse().unwrap()).unwrap();
-        // Create public key from  
-        let xpub = xprv.public_key();
-        
-        let xprv_str = xprv.to_string(Prefix::XPRV);
-        let xpub_str = xpub.to_string(Prefix::XPUB);
+        match Self::create_wallet(mnemonic, passphrase){
+            Ok(wallet) => Ok(wallet),
+            Err(e) => Err(WalletError::WalletCreationFailed)
+        }
+    }
 
-        println!("\n\n====== Private Public Key Pair=========");    
-        println!("xprv:  {:?}", xprv_str);
-        println!("xpub:  {:?}", xpub_str);
+    pub fn from_mnemonic(phrase: &str, passphrase: &str, language: Language) -> Result<Wallet, WalletError>{
+        let mnemonic = Mnemonic::parse_in(language, phrase)?;
 
-
-        // 
-        let signing_key = xprv.private_key();
-        let verfication_key = xpub.public_key();
-
-    
-        Wallet {
-            address: Self::derive_address(&xpub),
-            public_key: xpub,
-            private_key: xprv,
-            master_key: xprv_str,
-            mnemonic,
-            seed,
-            keys: 0,
-            balance: 0,
+        match Self::create_wallet(mnemonic, passphrase){
+            Ok(wallet) => Ok(wallet),
+            Err(e) => Err(WalletError::WalletCreationFailed)
         }
     }
 
@@ -102,7 +102,7 @@ impl Wallet {
         // To return details
     }
 
-    pub fn get_wallet(&self, address: &str) -> Result<Wallet, WalletErrors> {
+    pub fn get_wallet(&self, address: &str) -> Result<Wallet, WalletError> {
         unimplemented!()
     }
 
@@ -110,7 +110,6 @@ impl Wallet {
 
 // Implement private API
 impl Wallet {
-    #[instrument]
     pub fn derive_address(xpub: &KetoPublicKey) -> String {
         // Get compressed public key bytes (33 bytes)
         let pub_key_bytes = xpub.public_key().to_bytes();
@@ -129,5 +128,50 @@ impl Wallet {
         bs58::encode(payload).with_check().into_string()
     }
 
+    pub fn create_wallet(mnemonic: Mnemonic, passphrase: &str) -> Result<Wallet, WalletError>{
+ 
+        // Converts mnemonic seed to bytes. The passphrase is a string. It can take the value of "" if not provided by user
+        let seed = mnemonic.to_seed(passphrase); // 
+        
+        //let master_key =  XPrv::new(seed).unwrap();
+        
+        // The derivation path for private key creation
+        let path = "m/44'/9999'/0'/0/0";
+        // Create the private key
+        let xprv = XPrv::derive_from_path(seed, &path.parse().unwrap())?;
+        // Create public key from  
+        let xpub = xprv.public_key();
+        
+        let xprv_str = xprv.to_string(Prefix::XPRV);
+        let xpub_str = xpub.to_string(Prefix::XPUB);
+
+        println!("\n\n====== Private Public Key Pair=========");    
+        println!("xprv:  {:?}", xprv_str);
+        println!("xpub:  {:?}", xpub_str);
+
+
+        // let signing_key = xprv.private_key();
+        // let verfication_key = xpub.public_key();
+
+    
+        let new_wallet = Wallet {
+            address: Self::derive_address(&xpub),
+            public_key: xpub,
+            private_key: xprv,
+            master_key: xprv_str,
+            mnemonic,
+            seed,
+            addresses: 1,
+            balance: 0,
+        };
+
+        Ok(new_wallet)
+    }
+
+    fn create_address(&mut self) -> Result<String, WalletError>{
+        // let path = format!("m/44'/9999'/0'/0/{}", self.addresses);
+        // let xprv = self.private_key.derive_child(self.addresses)
+        unimplemented!()
+    }
 }
 // endection:   -- Struct and Enum declarations
